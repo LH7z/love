1f7f578ee08c9739d4be30be6ee833c86e2bb2e9
luis henrique <luisham2001@gmail.com>
2025-06-04 11:01:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+  const container = document.querySelector('.escritos');
+
+  function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = (Math.random() * 3 + 2) + 's'; // velocidade variável
+    heart.style.opacity = Math.random();
+
+    container.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 5000); // limpa o coração após a animação
+  }
+
+  setInterval(createHeart, 300); // frequência da chuva
